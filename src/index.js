7ea7a179c06570e2bbdb90c33e834868fa363c7c
Somayyeh094 <s.somayyeh.seyedi@gmail.com>
2025-05-10 14:18:45 +0330

    clarified context

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ function callApi(event) {
   let quoteInput = document.querySelector("#textInput");
   let apiKey = "71c9o8ef0370bd39a326b41301fb04bt";
   let context =
-    "Please generate inspirational and motivational quotes which are short sentences. Each quote is placed within double quotation marks. write each quote like this format: quote<br/>. Give a number to each quote";
+    "Please generate inspirational and motivational quotes which are short sentences. Each quote is placed within double quotation marks. write each quote like this format: quote<br/>. Give a number to each quote. Also only write quotes without any explanations";
   let prompt = `generate qoutes about ${quoteInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
   let output = document.querySelector("#output");
