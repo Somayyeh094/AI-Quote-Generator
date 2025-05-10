783b69992179336a47d863dde3525bdda2e0eb63
Somayyeh094 <s.somayyeh.seyedi@gmail.com>
2025-05-10 20:05:20 +0330

    updated ux (blink, hidden box,..)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 function generateQuote(response) {
-  console.log(response.data.answer);
+  output.classList.remove("blink");
   new Typewriter("#output", {
     strings: response.data.answer,
     autoStart: true,
@@ -13,12 +13,15 @@ function callApi(event) {
   let quoteInput = document.querySelector("#textInput");
   let apiKey = "71c9o8ef0370bd39a326b41301fb04bt";
   let context =
-    "Please generate inspirational and motivational quotes which are short sentences. Each quote is placed within double quotation marks. write each quote like this format: quote<br/>. Give a number to each quote. Also only write quotes without any explanations";
+    "Please generate inspirational and motivational quotes. Each quote is placed within double quotation marks. write each quote like this format: quote<br/>. Give a number to each quote. Also only write quotes without any explanations";
   let prompt = `generate qoutes about ${quoteInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-  let output = document.querySelector("#output");
-  output.innerHTML = `Generating quotes about "${quoteInput.value}"`;
+  let outputAi = document.querySelector("#outputAi")
+  outputAi.classList.remove("hidden");
+  output.classList.add("blink");
+  output.innerHTML = `<em>Generating quotes about "${quoteInput.value}". Please wait<em/> ....`;
   axios.get(apiUrl).then(generateQuote);
 }
+let output = document.querySelector("#output");
 let generateForm = document.querySelector("#generateForm");
 generateForm.addEventListener("submit", callApi);
